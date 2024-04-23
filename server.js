c28e2ac576f6afd1782e6644e961deb3f97ddb61
Ace <aceshyjan@gmail.com>
2024-04-23 14:37:15 +0100

    Added partials and started registration

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express'); 
+const bcrypt = require('bcrypt');
 const app = express();
+
+// Change this
+const users = [];
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false}));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -24,10 +29,48 @@ app.get('/team', (req, res) => {
     res.render('team.ejs');
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
+    if (req.body.password !== req.body.confirm) {
+        console.log('Passwords do not match');
+        return res.redirect('/register');
+    }
+
+    if (users.find(user => user.email === req.body.email)) {
+        console.log('Username already exists');
+        return res.redirect('/register');
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            username: req.body.email,
+            password: hashedPassword
+        });
+        console.log('User has been created');
+        res.redirect('/');
+    }
+    catch {
+        console.log('Error');
+        res.redirect('/register');
+    }
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
+    const user = users.find(user => user.email === req.body.email);
+    if (user == null) {
+        return res.redirect('/');
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.redirect('/dashboard');
+        } else {
+            res.redirect('/');
+        }
+    }
+    catch {
+        res.redirect('/');
+    }
 });
 
 app.listen(3000);
