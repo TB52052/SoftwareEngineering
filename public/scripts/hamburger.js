c1a9f98d08a5163c6a104dd1e97bbf4e32f4b2fb
Ace <aceshyjan@gmail.com>
2024-04-26 13:16:18 +0100

    Fixed sidebar animations

--- public/scripts/hamburger.js
+++ public/scripts/hamburger.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("hamburger-toggle").addEventListener("click", function() {
         this.classList.toggle("open"); // Toggle hamburger icon
         document.querySelector(".sidebar").classList.toggle("open"); // Toggle sidebar
+        document.querySelector(".sidebar-wrapper").classList.toggle("open"); // Toggle content
     });
 });
