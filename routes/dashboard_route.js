7bf4af1c20a291127bcf835c0f86064fdd9a49d0
SanshrayaPatro <yfv22cru@uea.ac.uk>
2024-04-30 19:02:06 +0100

    Semester Drop down menu

--- routes/dashboard_route.js
+++ routes/dashboard_route.js
@@ -1,7 +1,35 @@
+
 const express = require('express');
 const router = express.Router();
-module.exports = router;
+const sqlite3 = require('sqlite3').verbose();
 
-router.get('/', (req, res) => {
-    res.render('dashboard.ejs', { title: 'Dashboard'});
+
+const db = new sqlite3.Database('./db/semesterInfo.db');
+
+
+async function getSemester() {
+    return new Promise((resolve, reject) => {
+        db.all(`SELECT Season FROM semesterInfo`, [], (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
+
+
+router.get('/', async (req, res) => {
+    try {
+        const allSem = await getSemester();
+        console.log(allSem);
+        res.render('dashboard.ejs', { title: 'dashboard', semesterInfo: allSem });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+
+
+module.exports = router;
