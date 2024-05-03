eca236982889a6c47ba87530a366e08a10210e68
TB52052 <taylorbullard04@outlook.com>
2024-05-03 16:51:41 +0100

    Updated now receiving pop-up

--- public/scripts/fileManagement.js
+++ public/scripts/fileManagement.js
@@ -2,31 +2,29 @@ document.addEventListener("DOMContentLoaded", function(){
 
     document.getElementById('semesterForm').addEventListener('submit', function(event) {
         event.preventDefault(); 
-       
+
         var selectedSemester = document.getElementById('seasonForm').value;
         console.log('Selected Semester:', selectedSemester);
        
         var expectedFileName = getExpectedFileName(selectedSemester);
         console.log('Expected File Name:', expectedFileName);
-       });
+
+        checkFileName(expectedFileName);
+    });
 
 });
 
+function checkFileName(expectedFileName) {
+    var fileInput = document.getElementById('fileInputForm');
+    if (fileInput.files.length > 0) {
+        var uploadedFileName = fileInput.files[0].name;
 
-
-
-
-function checkFileName() {
-var fileInput = document.getElementById('fileInputForm'); // Corrected ID
-if (fileInput.files.length > 0) {
-var uploadedFileName = fileInput.files[0].name;
-
-if (uploadedFileName === expectedFileName) {
-alert("File name matches!");
-} else {
-alert("File name doesn't match.");
-}
-} else {
-alert("Please select a file.");
-}
+        if (uploadedFileName === expectedFileName) {
+            alert("File name matches!");
+        } else {
+            alert("File name doesn't match.");
+        }
+    } else {
+        alert("Please select a file.");
+    }
 }
