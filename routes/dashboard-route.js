5195ae918bff4be98c0f12f538e4009d1013624a
TB52052 <taylorbullard04@outlook.com>
2024-05-18 14:40:52 +0100

    Update using correct tables
    Comparison between .json file and db is used and matching userModules and userAssessments are added to the relevant tables

--- routes/dashboard-route.js
+++ routes/dashboard-route.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
         const allAssessments = await getAssessment();
         console.log("Assessments from Database:", allAssessments);
 
-        const filePath = `./json/${req.query.semester}.json`; // semsester is in query
+        const filePath = `./json/${req.query.semester}.json`; // semester is in query
 
         // read JSON file (dynamic)
         const jsonData = fs.readFileSync(filePath);
@@ -39,8 +39,41 @@ router.get('/', async (req, res) => {
 
         console.log("Matching Assessments:", matchingAssessments);
 
-        res.render('dashboard.ejs', { title: 'dashboard', AssessmentInfo: allAssessments });
+        const id = req.session.user.id;
 
+        // insert matching modules userModules Table
+        const insertStmtModule = db.prepare(`INSERT INTO userModules (UserID, ModuleID) VALUES (?, ?)`);
+        matchingAssessments.forEach(matchingAssessment => {
+            const { AssessmentID, ModuleID } = matchingAssessment;
+
+            // Insert operation
+            insertStmtModule.run([id, ModuleID], function(err) {
+                if (err) {
+                    console.error('Already Exisits in table');
+                } else {
+                    console.log('Record inserted successfully');
+                }
+            });
+        });
+        insertStmtModule.finalize();
+
+        // insert matching modules userModules Table
+        const insertStmtAssessment = db.prepare(`INSERT INTO UserAssessments (UserID, AssessmentID) VALUES (?, ?)`);
+        matchingAssessments.forEach(matchingAssessment => {
+            const { AssessmentID, ModuleID } = matchingAssessment;
+
+            // Insert operation
+            insertStmtAssessment.run([id, AssessmentID], function(err) {
+                if (err) {
+                    console.error('Already Exisits in table');
+                } else {
+                    console.log('Record inserted successfully');
+                }
+            });
+        });
+        insertStmtAssessment.finalize();
+
+        res.render('dashboard.ejs', { title: 'dashboard', AssessmentInfo: allAssessments });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
